0287c986e8f78a342f72d31fdee3eb5a683e16cb
Rockberpro <rockberpro@gmail.com>
2025-06-13 00:16:53 -0300

    Basic gui

--- main.py
+++ main.py
@@ -14,18 +14,38 @@ def center_window(window, width=300, height=200):
 def select_wav_file():
     file_path = filedialog.askopenfilename(
         filetypes=[("WAV files", "*.wav")],
-        title="Selecione um arquivo WAV"
+        title="Select a WAV file"
     )
     if file_path:
         print(f"Selected audio file: {file_path}")
 
+def encode():
+    # Placeholder for encoding logic
+    print("Encoding audio...")
+
+def decode():
+    # Placeholder for decoding logic
+    print("Decoding audio...")
+
+##
+ # Main application window
+##
 root = ttk.Window(title="Subliminal Audio Converter", size=(400, 120))
 center_window(root, 400, 120)
 
-encode_btn = ttk.Button(
+file_btn = ttk.Button(
     root, text="Select file", bootstyle=INFO, command=select_wav_file
+)
+file_btn.pack(side=LEFT, padx=20, pady=30, expand=True)
+
+encode_btn = ttk.Button(
+    root, text="Encode", bootstyle=SUCCESS, command=encode
 )
 encode_btn.pack(side=LEFT, padx=20, pady=30, expand=True)
 
-root.mainloop()
+decode_btn = ttk.Button(
+    root, text="Decode", bootstyle=WARNING, command=decode
+)
+decode_btn.pack(side=LEFT, padx=20, pady=30, expand=True)
 
+root.mainloop()
